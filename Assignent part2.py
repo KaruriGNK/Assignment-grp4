7fd1fbf1faf867ecad7c476786551b81f6909101
Kartech <dedankaruri11451@gmail.com>
2024-10-31 08:08:54 +0300

    Update Assignent part2.py

--- Assignent part2.py
+++ Assignent part2.py
@@ -1,13 +1,13 @@
 def fib(n):
-    # Base case: if n is 0, return a list with the first Fibonacci number
+    #If n is 0, return a list with the first Fibonacci number
     if n == 0:
         return [0]
 
-    # Base case: if n is 1, return a list with the first two Fibonacci numbers
+    #If n is 1, return a list with the first two Fibonacci numbers
     elif n == 1:
         return [0, 1]
 
-    # Recursive case: for n greater than 1
+    #for n greater than 1
     else:
         # Call the function recursively to get the Fibonacci sequence up to n-1
         lst = fib(n - 1)
